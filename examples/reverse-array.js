0adf2d75c5e7375f83c8f24a0ebb0796a22b43e7
Nick Meldrum <Nick.Meldrum@sc.com>
2020-03-05 05:17:39 +0800

    reverse array attempt

--- examples/reverse-array.js
+++ examples/reverse-array.js
@@ -1,18 +1,26 @@
 // reverse an array
 const { performance } = require('perf_hooks')
 
-const reverse = array => {}
+const reverse = array => {
+  for (let i = 0; i < array.length / 2; i += 1) {
+    const x = array[i]
+    array[i] = array[array.length - i]
+    array[array.length - i] = x
+  }
+}
 
 const makeArray = length => [...Array(length + 1).keys()].slice(1)
 
 module.exports = () => {
-  const arr = makeArray(100)
+  const arr = makeArray(4)
 
   let before
   let after
 
   before = performance.now()
+  console.log(arr)
   reverse(arr)
+  console.log(arr)
   after = performance.now()
   console.log(`naive reverse = ${after - before}`)
 }
